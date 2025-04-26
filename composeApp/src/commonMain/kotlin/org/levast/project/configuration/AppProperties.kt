43f1e86657fee2d9fcc6c773e9cc40a3de5eeb7b
emile <emilelevast@outlook.fr>
2025-04-26 23:54:59 +0200

    Utilise le port correct

--- composeApp/src/commonMain/kotlin/org/levast/project/configuration/AppProperties.kt
+++ composeApp/src/commonMain/kotlin/org/levast/project/configuration/AppProperties.kt
@@ -1,7 +1,7 @@
 package org.levast.project.configuration
 
 import kotlinx.serialization.Serializable
-import org.levast.project.SERVER_PORT
+import org.levast.project.SERVER_KTOR_PORT
 
 @Serializable
-class AppProperties(var ipAdressServer:String="", var userName:String = "",var isUserMode:Boolean?= null, val portServer: Int = SERVER_PORT)
+class AppProperties(var ipAdressServer:String="", var userName:String = "",var isUserMode:Boolean?= null, val portServer: Int = SERVER_KTOR_PORT)
