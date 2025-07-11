be065f16f121af353267f72b31153dffc944e7ff
emile <emilelevast@outlook.fr>
2025-07-11 17:45:33 +0200

    Use port 5000 to enable access Aws beanstalk

--- shared/src/commonMain/kotlin/org/levast/project/Constants.kt
+++ shared/src/commonMain/kotlin/org/levast/project/Constants.kt
@@ -1,5 +1,5 @@
 package org.levast.project
 
 const val SERVER_PORT = 8080
-const val SERVER_KTOR_PORT = 9090
+const val SERVER_KTOR_PORT = 5000
 const val ERROR_NETWORK_MESSAGE = "Le réseau semble inacessible : allume le serveur connard"
