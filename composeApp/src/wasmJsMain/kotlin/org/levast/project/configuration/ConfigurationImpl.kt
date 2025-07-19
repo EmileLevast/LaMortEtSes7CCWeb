9c31b06174adb103e439566d615130711eb0aee1
emile <emilelevast@outlook.fr>
2025-07-19 23:32:56 +0200

    configuration ssl site web

--- composeApp/src/wasmJsMain/kotlin/org/levast/project/configuration/ConfigurationImpl.kt
+++ composeApp/src/wasmJsMain/kotlin/org/levast/project/configuration/ConfigurationImpl.kt
@@ -1,14 +1,16 @@
 package org.levast.project.configuration
 
+import org.levast.project.SERVER_KTOR_PORT_SSL
+
 
 // At the top level of your kotlin file:
 
 class ConfigurationImpl() : IConfiguration {
 
     // TODO: Vérifier si cette adresse IP par défaut est toujours pertinente pour les environnements de test
-    private var properties: AppProperties = AppProperties("192.168.138.178","",)
+    private var properties: AppProperties = AppProperties("13.51.248.211", portServer = SERVER_KTOR_PORT_SSL)
 
-    override fun getEndpointServer() = "https://lesfeusperegrins-env.eba-gmrjrq6d.eu-north-1.elasticbeanstalk.com/"
+    override fun getEndpointServer() = "https://${properties.ipAdressServer}:${properties.portServer}"
 
     override fun getIpAdressTargetServer() =  properties.ipAdressServer
 
--- shared/src/commonMain/kotlin/org/levast/project/Constants.kt
+++ shared/src/commonMain/kotlin/org/levast/project/Constants.kt
@@ -2,6 +2,7 @@ package org.levast.project
 
 const val SERVER_PORT = 8080
 const val SERVER_KTOR_PORT = 5000
-const val SERVER_KTOR_PORT_SSL = 8443
+const val SERVER_KTOR_PORT_SSL = 443
+const val IP_ADRESS_SERVER = "13.51.248.211"
 const val ERROR_NETWORK_MESSAGE = "Le réseau semble inacessible : allume le serveur connard"
 const val SERVER_PATH_KEYSTORE_FILE = "certificate/keystore.jks"
