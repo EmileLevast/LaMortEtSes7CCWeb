0253c82641739ffc9a0046260b505a41f57a961b
emile <emilelevast@outlook.fr>
2025-08-16 18:42:29 +0200

    Ajout commentaire explicatif

--- composeApp/src/commonMain/kotlin/org/levast/project/configuration/AppProperties.kt
+++ composeApp/src/commonMain/kotlin/org/levast/project/configuration/AppProperties.kt
@@ -4,6 +4,11 @@ import kotlinx.serialization.Serializable
 import org.levast.project.SERVER_KTOR_PORT
 import org.levast.project.SERVER_KTOR_PORT_SSL
 
+/**
+ * userName : c'est le nom du joueur que l'utilisateur veut voir dans l'application, basiquement c'est lui-même pour suivre le jeu, mais pas forcement (le MJ peut aimer naviguer vers d'autres joueurs)
+ * userAuthentication : contient le nom de l'utilisateur de l'application et son mot de passe associé.
+ * C'est en cours de dev mais l'idée est qu'un utilisateur ne puisse faire des commandes put que sur le profil de joueur associé
+ */
 @Serializable
 class AppProperties(var adressServer:String="", var userName:String = "", var isUserMode:Boolean?= null, var isHttpsOn : Boolean = true, var userAuthentication: UserAuthentication? = null){
 
