4104df2bf67755efda171dac9bcaf43be17a3a1b
emile <emilelevast@outlook.fr>
2025-12-07 17:15:31 +0100

    Dialog de suppression de compte

--- composeApp/src/commonMain/kotlin/org/levast/project/affichageMobile/EcranCompteUtilisateur.kt
+++ composeApp/src/commonMain/kotlin/org/levast/project/affichageMobile/EcranCompteUtilisateur.kt
@@ -3,8 +3,10 @@ package org.levast.project.affichageMobile
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
@@ -15,6 +17,10 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
+import org.levast.project.model.CompteUtilisateur
 import org.levast.project.viewModel.CompteUtilisateurViewModel
 
 @Composable
@@ -22,20 +28,47 @@ fun EcranCompteUtilisateur(
     compteUtilisateurViewModel: CompteUtilisateurViewModel = viewModel { CompteUtilisateurViewModel() }
 ) {
 
-    //FIXME là y'a un soucis d'archi, on utilise une variable pour declencher le launchedEffect qui lui meme declenche pour declencher une variable, faut raccroucir la chaine
-    var getAllCompteUtilisateur by remember { mutableStateOf(true) }
+    var isShowingDeleteDialog by remember { mutableStateOf<CompteUtilisateur?>(null) }
+    var isShowingUpdateCard by remember { mutableStateOf<CompteUtilisateur?>(null) }
+    var isShowingInsertCard by remember { mutableStateOf<CompteUtilisateur?>(null) }
     val compteUtilisateurUiState by compteUtilisateurViewModel.uiStateAllComptes.collectAsState()
 
-    LaunchedEffect(getAllCompteUtilisateur) {
+    remember {
         compteUtilisateurViewModel.getAllComptesRequest()
     }
 
     LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)) {
         items(compteUtilisateurUiState) {
-            Card{
-                Text(it.nom, fontWeight = FontWeight.Bold )
+            Card {
+                Text(it.nom, fontWeight = FontWeight.Bold)
                 Text(it.motDePasse)
             }
         }
+    }
+
+    isShowingDeleteDialog?.let { compte ->
+        AlertDialog(
+            title = { Text("Supprimer ${compte.nom}") },
+            onDismissRequest = { isShowingDeleteDialog = null },
+            confirmButton = {
+                TextButton(
+                    onClick = {
+                        compteUtilisateurViewModel.deleteCompte.sendRequest(compte)
+                        isShowingDeleteDialog = null
+                    }
+                ) {
+                    Text("Supprimer")
+                }
+            },
+            dismissButton = {
+                TextButton(
+                    onClick = {
+                        isShowingDeleteDialog = null
+                    }
+                ) {
+                    Text("Annuler")
+                }
+            }
+        )
     }
 }
