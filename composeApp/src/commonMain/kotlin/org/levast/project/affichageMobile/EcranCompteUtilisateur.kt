4a2ed44ceef85a7ba81fb2582af520d1738471d8
emile <emilelevast@outlook.fr>
2025-12-07 17:51:34 +0100

    Integration IHm pour creer des comptes utilisateurs et les supprimer et autre

--- composeApp/src/commonMain/kotlin/org/levast/project/affichageMobile/EcranCompteUtilisateur.kt
+++ composeApp/src/commonMain/kotlin/org/levast/project/affichageMobile/EcranCompteUtilisateur.kt
@@ -1,15 +1,23 @@
 package org.levast.project.affichageMobile
 
+import CHAR_SEP_EQUIPEMENT
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.rounded.Delete
+import androidx.compose.material.icons.rounded.Edit
 import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextField
@@ -41,58 +49,125 @@ fun EcranCompteUtilisateur(
         compteUtilisateurViewModel.getAllComptesRequest()
     }
 
-    LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp), modifier = Modifier.fillMaxSize()) {
-        items(compteUtilisateurUiState) {
-            Card (modifier = Modifier.padding(2.dp)){
-                Text(it.nom, fontWeight = FontWeight.Bold)
-                Text(it.motDePasse)
-                Text(it.roles.joinToString(","))
+    Box(Modifier.fillMaxSize(), contentAlignment = Alignment.Center) {
+
+        Column(Modifier.fillMaxSize(), horizontalAlignment = Alignment.CenterHorizontally) {
+
+            TextButton(
+                onClick = {
+                    if(isShowingInsertCard == null){
+                        isShowingInsertCard = CompteUtilisateur("", "", listOf())
+                    }
+                }
+            ) {
+                Text("Nouveau compte")
             }
+
+            LazyVerticalGrid(
+                columns = GridCells.Adaptive(minSize = 128.dp),
+            ) {
+                items(compteUtilisateurUiState) {
+                    Card(modifier = Modifier.padding(2.dp)) {
+                        Row (Modifier.fillMaxWidth(),horizontalArrangement = Arrangement.Center){
+                            Text(it.nom, fontWeight = FontWeight.Bold, modifier =  Modifier.weight(1f))
+
+                            IconButton(onClick = {
+                                isShowingDeleteDialog = it
+                            })
+                            {
+                                Icon(Icons.Rounded.Delete, "Supprimer compte")
+                            }
+                            IconButton(onClick = {
+                                isShowingUpdateCard = it
+                            })
+                            {
+                                Icon(Icons.Rounded.Edit, "editer compte")
+                            }
+                        }
+
+                        Text(it.motDePasse)
+                        Text(it.roles.joinToString(","))
+                    }
+                }
+            }
+
+
+        }
+
+        isShowingDeleteDialog?.let { compte ->
+            AlertDialog(
+                title = { Text("Supprimer ${compte.nom}") },
+                onDismissRequest = { isShowingDeleteDialog = null },
+                confirmButton = {
+                    TextButton(
+                        onClick = {
+                            compteUtilisateurViewModel.deleteCompte.sendRequest(compte)
+                            isShowingDeleteDialog = null
+                        }
+                    ) {
+                        Text("Supprimer")
+                    }
+                },
+                dismissButton = {
+                    TextButton(
+                        onClick = {
+                            isShowingDeleteDialog = null
+                        }
+                    ) {
+                        Text("Annuler")
+                    }
+                }
+            )
+        }
+
+        isShowingUpdateCard?.let {
+            CardCompteUtilisateur(
+                it,
+                confirm = {
+                        compteUtilisateurViewModel.updateCompte.sendRequest(it)
+                    isShowingUpdateCard = null
+                },
+                cancel = {
+                    isShowingUpdateCard = null
+                }
+            )
+        }
+
+        isShowingInsertCard?.let {
+            CardCompteUtilisateur(
+                it,
+                confirm = {
+                    compteUtilisateurViewModel.insertCompte.sendRequest(it)
+                    isShowingInsertCard = null
+                },
+                cancel = {
+                    isShowingInsertCard = null
+                }
+            )
         }
     }
 
-    isShowingDeleteDialog?.let { compte ->
-        AlertDialog(
-            title = { Text("Supprimer ${compte.nom}") },
-            onDismissRequest = { isShowingDeleteDialog = null },
-            confirmButton = {
-                TextButton(
-                    onClick = {
-                        compteUtilisateurViewModel.deleteCompte.sendRequest(compte)
-                        isShowingDeleteDialog = null
-                    }
-                ) {
-                    Text("Supprimer")
-                }
-            },
-            dismissButton = {
-                TextButton(
-                    onClick = {
-                        isShowingDeleteDialog = null
-                    }
-                ) {
-                    Text("Annuler")
-                }
-            }
-        )
-    }
 }
 
 @Composable
 fun CardCompteUtilisateur(
-    compteUtilisateur: CompteUtilisateur?,
-    confirm:(CompteUtilisateur?)->Unit,
-    cancel:()->Unit
-){
+    compteUtilisateur: CompteUtilisateur,
+    confirm: (CompteUtilisateur) -> Unit,
+    cancel: () -> Unit
+) {
 
-    var compteToSend by remember { mutableStateOf<CompteUtilisateur>(compteUtilisateur ?: CompteUtilisateur("", "", listOf())) }
+    var compteToSend by remember {
+        mutableStateOf<CompteUtilisateur>(
+            compteUtilisateur
+        )
+    }
 
     Column(
         Modifier.fillMaxSize().padding(20.dp),
         verticalArrangement = Arrangement.Center,
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
-        Card (modifier = Modifier.padding(10.dp)){
+        Card(modifier = Modifier.padding(10.dp)) {
             TextField(
                 value = compteToSend.nom,
                 onValueChange = { compteToSend = compteToSend.copy(nom = it) },
@@ -105,7 +180,7 @@ fun CardCompteUtilisateur(
                 value = compteToSend.roles.joinToString(","),
                 onValueChange = { compteToSend = compteToSend.copy(roles = it.split(",")) },
                 label = { Text("Roles (séparés par des virgules)") })
-            Row{
+            Row {
                 TextButton(onClick = {
                     confirm(compteUtilisateur)
                 }) {
