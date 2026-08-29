7e3dee95fd22c388895c13a2171ec3d6bd57710a
emile <emilelevast@outlook.fr>
2026-08-29 13:18:38 +0200

    feat : ajout de multiples messages d'erreurs

--- composeApp/src/commonMain/kotlin/org/levast/project/network/ApiApp.kt
+++ composeApp/src/commonMain/kotlin/org/levast/project/network/ApiApp.kt
@@ -352,7 +352,7 @@ class ApiApp(val config: IConfiguration, val notification: NotificationRepositor
                     when (it.status) {
                         HttpStatusCode.Unauthorized -> notification.sendNotification("Erreur  d'authentification ${it.status}")
                         HttpStatusCode.Forbidden -> notification.sendNotification("Erreur d'autorisation ${it.status}")
-                        HttpStatusCode.ExpectationFailed -> notification.sendNotification("Erreur  ${it.status}")
+                        HttpStatusCode.ExpectationFailed -> notification.sendNotification("Erreur de conformité ${it.status}")
                         HttpStatusCode.NotFound -> notification.sendNotification("Erreur de recherche ${it.status}")
                         HttpStatusCode.InternalServerError -> notification.sendNotification("Erreur serveur ${it.status}")
                         HttpStatusCode.BadRequest -> notification.sendNotification("Erreur de formatage ${it.status}")
@@ -362,12 +362,13 @@ class ApiApp(val config: IConfiguration, val notification: NotificationRepositor
                         HttpStatusCode.RequestTimeout -> notification.sendNotification("Erreur de timeout ${it.status}")
                         HttpStatusCode.GatewayTimeout -> notification.sendNotification("Erreur de timeout ${it.status}")
                         HttpStatusCode.ServiceUnavailable -> notification.sendNotification("Erreur de service indisponible ${it.status}")
-                        HttpStatusCode.NotModified -> notification.sendNotification("Erreur Aucune mise à jour effectuée ${it.status}")
+                        HttpStatusCode.NotModified -> notification.sendNotification("Erreur aucune mise à jour effectuée ${it.status}")
                         HttpStatusCode.NotAcceptable -> notification.sendNotification("Impossible de supprimer ${it.status}")
                         HttpStatusCode.UnprocessableEntity -> notification.sendNotification("Impossible de créer ${it.status}")
                         HttpStatusCode.NoContent -> notification.sendNotification("Rien trouvé")
                         HttpStatusCode.Created -> notification.sendNotification(messageOk)
                         HttpStatusCode.OK -> notification.sendNotification(messageOk)
+                        else -> notification.sendNotification("Erreur ${it.status}")
                     }
                 }
             }
